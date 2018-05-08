a82edacb615808925745a3853a43e0fd92340b9f
Akhilesh Dhaundiyal <akhilesh.dhaundiyal@gmail.com>
2018-05-08 14:36:08 +0530

    Update adminClass.java

--- adminClass.java
+++ adminClass.java
@@ -1,7 +1,7 @@
 import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import jxl.Sheet;
@@ -9,19 +9,18 @@ import jxl.Workbook;
 
 public class Test
 {
-	private uploadXls(String fileName)
+	private static void uploadXls(String fileName)
 	{
 		try 
 		{
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","root");
-			Statement st=con.createStatement();
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","akd","haha");
+			Statement st = con.createStatement();
 			
 			File file = new File(fileName);
 			Workbook workbook = Workbook.getWorkbook(file);  
 		    Sheet sh = workbook.getSheet(0);
 		    
-		    String q, op1, op2, op3, op4, str;
-		    int ans, subId;
+		    String q, op1, op2, op3, op4, str, ans, subId;
 		    
 		    for (int rows = 0; rows < sh.getRows(); rows++) 
 		    {	        
@@ -32,36 +31,57 @@ public class Test
 		    	op4 = sh.getCell(4, rows).getContents();
 		    	ans = sh.getCell(5, rows).getContents();
 		    	subId = sh.getCell(6, rows).getContents();
+
 		    	
 		    	str = "insert into questions"+ " (questionName, option1, option2, option3, option4, answerNo, subId) values ('"
-		    			+ q+ "', '"+ op1+ "', '"+ op2+ "', '"+ op3+ "', '"+ op4+ "', "+ ans+ ", "+ subId+ ");";
-		    	//System.out.println(str);
-		    	st.executeUpdate(str);	    	
+		    			+ q+ "', '"+ op1+ "', '"+ op2+ "', '"+ op3+ "', '"+ op4+ "', "+ ans+ ");";		    	
+		    	st.executeUpdate(str);
+		    	
+		    	
+		    	String[] atomicSubId = subId.split("\\s+");		    	
+		    	str = "select max(questionId) from questions";
+		    	ResultSet rs = st.executeQuery(str);		    
+		    	rs.next();
+		    	int qId = rs.getInt(1);
+		    	
+		    	for(int i=0; i<atomicSubId.length; i++)
+		    	{
+		    		str = "insert into subjectquestionmapping"+ " (subId, questionId) values ("
+		    				+ atomicSubId[i]+ ", "+ qId+ ");";
+		    		st.executeUpdate(str);
+		    	}
+		    	
 		    } 
 			con.close();
 		}
-		catch (IOException e) 
+		catch (Exception e) 
 			{e.printStackTrace();}
 	}
 
-	private deleteQuestion(int qId)
+	private static void deleteQuestion(int qId)
 	{
 		try 
 		{
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","root");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","akd","haha");
 			Statement st=con.createStatement();			
 			String str;
 
 			str = "delete from questions where questionId="+ qId;
-		    st.executeUpdate(str);	    			    
+		    st.executeUpdate(str);	    			
+		    str = "delete from subjectquestionmapping where questionId="+ qId;
+		    st.executeUpdate(str);
+		    
 			con.close();
 		}
-		catch (IOException e) 
+		catch (Exception e) 
 			{e.printStackTrace();}
 	}
 
 	public static void main(String[] args) throws Exception 
-	{		
-		//code;
-    	}  
+	{			
+		String file = "C:\\Users\\Training\\Desktop\\test.xls";
+		int questionId = 32;
+		//uploadXls(file);
+		//deleteQuestion(questionId);
+    }  
 }
